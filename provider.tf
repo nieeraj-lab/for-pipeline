d905d79464b97172377b21695d73243222856ca4
Neeraj Kumar <neeraj1.jnv@gmail.com>
2025-07-27 08:16:31 +0530

    Code ko add kiya h

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.37.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "4874f019-c697-4254-8925-749b8f7c89e5"
+}
